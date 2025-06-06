1123dfd93500569e4d56d185a54674a8b97a2165
plan28-06 <planar2006@gmail.com>
2025-06-06 14:26:49 +0530

    Done with /new page.

--- app.js
+++ app.js
@@ -18,13 +18,27 @@ const messages = [
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index",{messages:messages});
+    res.render("index", { messages: messages });
 });
 
 app.get("/new", (req, res) => {
-    res.send("<h1>Form</h1>");
+    res.render("form");
+});
+
+app.post("/new", (req, res) => {
+    if (!req.body ||!req.body.text || !req.body.user ) {
+        return res.status(400).send("<h1>Yo please send data correctly</h1>");
+    }
+    const newpost = {
+        text: req.body.text,
+        user: req.body.user,
+        added: new Date(),
+    };
+    messages.push(newpost);
+    res.redirect("/");
 });
 
 app.listen(PORT, () => {
