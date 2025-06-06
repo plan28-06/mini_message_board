53b2876180902ff6d478e5ef4612684016bf2b82
plan28-06 <planar2006@gmail.com>
2025-06-06 09:40:25 +0530

    Basic Setup Completed.

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("node:path");
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home</h1>");
+});
+
+app.get("/new", (req, res) => {
+    res.send("<h1>Form</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Started Server on PORT ${PORT}`);
+});
